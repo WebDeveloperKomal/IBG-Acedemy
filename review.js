f2d566fa345ca55805d0ad6b0292fc721ed9ac51
WebDeveloperKomal <76107750+WebDeveloperKomal@users.noreply.github.com>
2023-12-06 22:40:25 -0800

    Add files via upload

--- review.js
+++ review.js
@@ -32,16 +32,16 @@ function saveReview() {
 
 }
 
-<<<<<<< HEAD
+
 // ---------------------------GET------------------------------------------------
 
 
 
 fetch('http://localhost:8080/dummy', {
     method: 'PUT',
-    body: JSON.stringify({
-        id: 1,
-=======
+    body: JSON.stringify,
+    id: 1,
+})
 // ---------------------------------------------------------------------------------
 
 
@@ -49,27 +49,26 @@ fetch('http://localhost:8080/dummy', {
 fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
->>>>>>> ef931d82159c1449860385fa3d0375e45fb2a415
+
         title: 'foo',
         body: 'bar',
         userId: 1,
     }),
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
-    },
+    }
+
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+
 })
-    .then((response) => response.json())
-<<<<<<< HEAD
-    .then((json) => console.log(json));
-
-
 
 // -------------------------DELETE---------------------------------------------
 
 
 fetch('http://localhost:8080/dummy', {
     method: 'DELETE',
-});  
-=======
-    .then((json) => console.log(json));  
->>>>>>> ef931d82159c1449860385fa3d0375e45fb2a415
+});
+
+
+
